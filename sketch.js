5a2c50fb244984b0f32f4d1c4365d89c5c8bb53e
aravan <82102020+aravns@users.noreply.github.com>
2022-01-23 22:11:27 +0300

    v0.01

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var blob;
+
+var blobs = [];
+
+function setup() {
+    createCanvas(600, 600);
+    blob = new Blob(width/2, height/2, 64);
+    for (var i = 0; i < 50; i++) {
+        var x = random(-width, width*2);
+        var y = random(-height, height*2);
+        blobs[i] = new Blob(x, y, 16);
+    }
+}
+
+function draw() {
+    background(0);
+
+    translate(width/2-blob.pos.x, height/2-blob.pos.y);
+    
+    for (var i = blob.length-1; i >= 0; i--) {
+        blobs[i].show;
+        if (blob.eats(blobs[i])) {
+            blobs.splice(i, 1)
+        }
+        blobs[i].show();
+    }
+    
+    
+    blob.show();
+    blob.update();
+}
+
